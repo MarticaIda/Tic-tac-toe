7e25b7d7e5741fa8ffaf96442ac10608b924620d
Maria Martinkous <marticaida@gmail.com>
2023-02-24 19:03:15 -0600

    hide all elements inside objects

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-window.onload = function () {
-  const gameboard = document.querySelectorAll('.cell')
-  const [c1, c2, c3, c4, c5, c6, c7, c8, c9] = gameboard
+function Gameboard () {
+  const board = document.querySelectorAll('.cell')
+  // gameboard.forEach((cell) => (cell.textContent = ''))
+  const [c1, c2, c3, c4, c5, c6, c7, c8, c9] = board
   const winningCombos = [
     [c1.id, c2.id, c3.id],
     [c4.id, c5.id, c6.id],
@@ -11,67 +12,60 @@ window.onload = function () {
     [c1.id, c5.id, c9.id],
     [c3.id, c5.id, c7.id]
   ]
+  const getBoard = () => board
+  const getWinningCombos = () => winningCombos
+  return {
+    getBoard,
+    getWinningCombos
+  }
+}
 
-  // players
-  const playerX = 'Player X'
-  const playerO = 'Player O'
+const Players = (playerX = 'Player X', playerO = 'Player O') => {
   const players = [
-    { name: playerX, piece: 'x' },
-    { name: playerO, piece: 'o' }
+    { name: playerX, marker: 'X', moves: [] },
+    { name: playerO, marker: 'O', moves: [] }
   ]
   let activePlayer = players[0]
   const switchPlayer = () => {
     activePlayer = activePlayer === players[0] ? players[1] : players[0]
   }
-  function getActivePlayer () {
-    return activePlayer
+
+  const getActivePlayer = () => activePlayer
+
+  return {
+    switchPlayer,
+    getActivePlayer
   }
+}
 
-  let playerXMoves = []
-  let playerOMoves = []
-
+function gameController () {
+  const board = Gameboard()
+  const player = Players()
   const result = document.getElementById('result')
-  // placing game pieces
 
-  gameboard.forEach((cell) => {
+  board.getBoard().forEach((cell) => {
     cell.addEventListener('click', makeMove)
     function makeMove () {
-      getActivePlayer()
       if (cell.textContent === '') {
-        if (activePlayer === players[0]) {
-          playerXMoves.push(cell.id)
-          cell.textContent = 'X'
-        } else if (activePlayer === players[1]) {
-          playerOMoves.push(cell.id)
-          cell.textContent = 'O'
-        }
+        player.getActivePlayer().moves.push(cell.id)
+        cell.textContent = player.getActivePlayer().marker
       }
       // checking against winning combinations
-      winningCombos.forEach((combo) => {
-        const match = combo.every((elem) => playerXMoves.includes(elem))
+      board.getWinningCombos().forEach((combo) => {
+        const match = combo.every((elem) =>
+          player.getActivePlayer().moves.includes(elem)
+        )
         if (match) {
-          // stop the game, reset gameboard, +clear player moves arrays
-          playerXMoves = []
-          playerOMoves = []
-          result.textContent = 'X wins'
+          result.textContent = `${player.getActivePlayer().name} wins`
+          // stop the game, clear player moves arrays
+          player.getActivePlayer().moves = []
         }
       })
-      winningCombos.forEach((combo) => {
-        const match = combo.every((elem) => playerOMoves.includes(elem))
-        if (match) {
-          playerXMoves = []
-          playerOMoves = []
-          result.textContent = 'O wins'
-        }
-      })
-      if (result.textContent.includes('win')) {
-        console.log('shit')
-        gameboard.forEach((cell) => (cell.textContent = ''))
-        result.textContent = ''
-      }
-      switchPlayer()
-      console.log(playerXMoves)
-      console.log(playerOMoves)
+      player.switchPlayer()
+      console.log(
+        `${player.getActivePlayer().name} ` + player.getActivePlayer().moves
+      )
     }
   })
 }
+const game = gameController()
